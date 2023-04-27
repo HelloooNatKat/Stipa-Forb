d1947430ab7ac557ccaa99b6aff1b148e1d3382e
HelloooNatKat <n.kataoka12@gmail.com>
2023-04-27 13:19:25 -0700

    megacomp data to stipa-forb, new prelim figures

--- data_cleaning/QAQC.R
+++ data_cleaning/QAQC.R
@@ -1,5 +1,4 @@
 source("data_cleaning/merge_stipa_brho_data.R")
-#Warning: bkgrd == c("BRHO", "Control") longer object length is not a multiple of shorter object length
 
 #SE Function ####
 calcSE<-function(x){
@@ -7,180 +6,15 @@ calcSE<-function(x){
   sd(x2)/sqrt(length(x2))
 }
 
-colnames(all.bkgrd)
-unique(all.bkgrd$block)
-#block 11 is not a megacomp block 
+no_total_biomass <- all.bkgrd %>%
+  filter(is.na(total.biomass.rounded.percap))
 
 ggplot(all.bkgrd, aes(x=phyto, y=total.biomass.rounded.percap)) + 
   geom_boxplot() 
 
-ggsave("all.bkgrd.png", width = 8, height = 5)
-
-no_total_biomass <- all.bkgrd %>%
-  filter(is.na(total.biomass.rounded.percap)) 
-#15 rows don't have total biomass 
-  #remove AVBA
-  #LENI still needs processing
-  #MAEL still needed to be processed
-  #remove ones no phytos
-
-ggplot(all.bkgrd, aes(x=phyto, y=total.biomass.rounded.percap, color=bkgrd)) + 
-  geom_boxplot() 
-
-ggsave("all.bkgrd.treatments.png", width = 8, height = 5)
-#notes
-  #remove MICA
-  #remove the "I" from THIR and TWIl in Bromus and Control background
-  #missing LENI control 
-  #MAEL still being processed 
-
-new.bkgrd <- all.bkgrd %>%
-  group_by(treatment, bkgrd, phyto) %>%
-  summarise(reps = n())
-
-Stipa.bkgrd <- all.bkgrd %>%
-  group_by(treatment, bkgrd, phyto, dens) %>%
-  filter(bkgrd == "Stipa") %>%
-  summarise(reps = n())
-
-Control.bkgrd <- all.bkgrd %>%
-  group_by(treatment, bkgrd, phyto, dens) %>%
-  filter(bkgrd == "Control") %>%
-  summarise(reps = n())
-
-BRHO.bkgrd <- all.bkgrd %>%
-  group_by(treatment, bkgrd, phyto, dens) %>%
-  filter(bkgrd == "BRHO") %>%
-  summarise(reps = n())
-
-#group_by = looking for unique combination of the variables you list
-# n = counts the number of observations 
-
-#Remove replicates with less than 3
-final.bckrd <- left_join(all.bkgrd, new.bkgrd, by = c("treatment", "bkgrd", "phyto")) %>%
-  filter(reps > 2)
-
-ggplot(BRHO.bkgrd, aes(x=phyto, y=reps, color=dens)) + 
-  geom_point() +
-  facet_wrap(~treatment)
-#you use ~ when using facet_wrap
-  ###for group meeting, see what we have enough of in H and L density, which species these are
-      ###have a graph of each species, use all.bkgrd
-ggplot(Stipa.bkgrd, aes(x=phyto, y=reps)) + 
-  geom_point() +
-  facet_wrap(~treatment)
-
-ggplot(Control.bkgrd, aes(x=phyto, y=reps)) + 
-  geom_point() +
-  facet_wrap(~treatment)
-
-ggsave("BRHO.bkgrd.replicate.density.png", width = 8, height = 5)
-
-#INDIVIDUAL SPECIES
-
-summary.all.bkgrd <- all.bkgrd %>%
-  group_by(treatment, bkgrd, phyto, origin, functional_group) %>%
-  summarize(mean.biomass = mean(total.biomass.rounded.percap, na.rm = TRUE),
-            se.biomass = calcSE(total.biomass.rounded.percap), 
-            n = length(total.biomass.rounded.percap))
-#summarize = creating a new column 
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "ACAM",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("ACAM")
-#[rows, columns]
-ggsave("ACAM.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "ANAR",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("ANAR")
-#[rows, columns]
-ggsave("ANAR.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "BRHO",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("BRHO")
-#[rows, columns]
-ggsave("BRHO.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "GITR",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("GITR")
-#[rows, columns]
-ggsave("GITR.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "LENI",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("LENI")
-#[rows, columns]
-ggsave("LENI.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "LOMU",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("LOMU")
-#[rows, columns]
-ggsave("LOMU.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "MAEL",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("MAEL")
-#[rows, columns]
-ggsave("MAEL.png", width = 8, height = 5)
-#weird we're getting se lines with only 2 points of data?
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "PLER",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("PLER")
-#[rows, columns]
-ggsave("PLER.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "THIR",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("THIR")
-#[rows, columns]
-ggsave("THIR.png", width = 8, height = 5)
-
-ggplot(summary.all.bkgrd[summary.all.bkgrd$phyto == "TWIL",], aes(x=bkgrd, y=mean.biomass, color=treatment)) + 
-  geom_point() +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  ggtitle("TWIL")
-#[rows, columns]
-ggsave("TWIL.png", width = 8, height = 5)
+ggplot(all.bkgrd, aes(x=treatment, y=total.biomass.rounded.percap, color=bkgrd)) + 
+  geom_boxplot() +
+  facet_wrap(~phyto, scales="free")
+#check for LENI; it's missing Stipa 
 
 
-#COMPARE ALL VARIABLES
-
-summary.all.bkgrd.origin <- all.bkgrd %>%
-  group_by(treatment, bkgrd, origin, functional_group) %>%
-  summarize(mean.biomass = mean(total.biomass.rounded.percap, na.rm = TRUE), se.biomass = calcSE(total.biomass.rounded.percap))
-
-ggplot(summary.all.bkgrd.origin, aes(x=bkgrd, y=mean.biomass, color=origin, shape = functional_group)) +
-  geom_point(size = 3) +
-  geom_errorbar(aes(ymin = mean.biomass-se.biomass, ymax = mean.biomass + se.biomass), width = 0.25) +
-  facet_wrap(~treatment)
-#[rows, columns]
-ggsave("all.variables.png", width = 8, height = 5)
-
-#ORIGIN x TREATMENT
-
-#FUNCTIONAL GROUP x TREATMENT
-
-
-#TO-DO
-#look for outliers in plotted data: visualize each species individually; take MAEL out to compare other species 
-
-#replot with the most up to date data (use final.bkgrd dataframe)
-  #compare functional group and treatment and bkgrd and origin 
-#make a model to see if density has any affect on biomass
-  #talk to group about this
-#look for samples that still need to be processed 
-#before 12/5 have new graphs 
--- data_cleaning/merge_stipa_brho_data.R
+++ data_cleaning/merge_stipa_brho_data.R
@@ -1,15 +1,33 @@
-
 # read in data ####
 #set working directory to Downloads/Stipa-Forb
 ## first, stipa backgrounds
-source("data_cleaning/stipa_initial_clean.R")
+source("data_cleaning/stipa_initial_clean.R") #this will clear the entire environment, put it first
 
-## then brho & control backgrounds
-source("data_cleaning/brho-control_initial_clean.R")
+if(file.exists("/Users/carme/Dropbox (University of Oregon)/MegaComp_Stipa_Overlap/Data/MegaComp_Data/")){
+  # Carmen
+  lead <- "/Users/carme/Dropbox (University of Oregon)/MegaComp_Stipa_Overlap/Data/MegaComp_Data/"
+  
+} else {
+  # Nat
+  lead <- "/Users/natkataoka/Dropbox/MegaComp_Stipa_Overlap/Data/MegaComp_Data/"
+}
+
+brho_control_clean <- read.csv(paste0(lead,"brho_control_bkgrd_20230419.csv"))
+
+#check to see if columns match between datasheets
+colnames(brho_control_clean)
+colnames(stipa.background.merged)
+  
+brho_control_final <- brho_control_clean %>%
+  select(-X, -unique.ID)
+
+stipa.background.merged_final <- stipa.background.merged %>% 
+  filter(complete.sample != "N") %>%
+  select(-complete.sample, -total.stem.length.mm.percap)
 
 # merge data frames ####
 
-## first, define sp functional group/origin
+## first, define sp functional group/origin; creating vectors
 native <- c("TWIL", "PLER", "LENI", "GITR", "ACAM", "MAEL")
 non_native <- c("ANAR", "BRHO", "LOMU", "THIR")
 
@@ -18,12 +36,14 @@ forb <- c("TWIL", "PLER", "LENI", "GITR", "ACAM", "MAEL", "ANAR", "THIR")
 
 drought <- c(1, 3, 4, 6, 12, 14)
 
-## merge
-all.bkgrd <- rbind(stipa.background.merged,BC.background.merged) %>%
+## merge; rbind is adding more rows on the bottom using similar column names
+all.bkgrd <- rbind(stipa.background.merged_final,brho_control_final) %>%
   mutate(treatment = ifelse(block %in% drought, "D", "C")) %>%
   filter(phyto.n.indiv != 0) %>%
   mutate(functional_group = ifelse(phyto %in% grass, "grass","forb"),
          origin = ifelse(phyto %in% native, "native", "non_native"))
+
+rm(brho_control_clean, brho_control_final, stipa.background.merged, stipa.background.merged_final)
 
 #you don't need quotations for a number
 #you need == because it's a logical statement (ifelse)
